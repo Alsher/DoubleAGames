e85ee914ed0f1ee1fa9667512f299cf94e108fde
Alsher <philipp.friese@hotmail.de>
2013-10-12 14:26:15 +0200

    refactor of repo name, changes in JWJGL2D-RPG

---
+++ LWJGL2D-RPG/src/doubeagamesdev/_2D/RPG/Game.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doubeagamesdev._2D.RPG;
+
+import com.doubleagamesdev.engine.GameObject;
+import com.doubleagamesdev.game.gameobject.Player;
+import java.util.ArrayList;
+import org.lwjgl.opengl.Display;
+
+/**
+ *
+ * @author Philipp Friese
+ */
+public class Game 
+{
+    private ArrayList<GameObject> objects;
+    private Player player; 
+    
+    public Game()
+    {
+        objects = new ArrayList<GameObject>();
+        
+        player = new Player(Display.getWidth() / 2 - Player.SIZE / 2, Display.getHeight() / 2 - Player.SIZE / 2);
+        
+        objects.add(player);
+        
+    }
+    
+    
+    public void getInput()
+    {
+        player.getInput();
+    }
+    
+    public void update()
+    {
+        for(GameObject go : objects)
+            go.update();
+    }
+    
+    public void render()
+    {
+        for(GameObject go : objects)
+            go.render();
+       
+    }
+}
