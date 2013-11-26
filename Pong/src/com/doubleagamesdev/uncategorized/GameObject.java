6e485aa1a47729344a3af5577764db18202eb504
Philipp Friese <philipp.friese@me.com>
2013-11-26 17:11:09 +0100

    changes to git

---
+++ Pong/src/com/doubleagamesdev/uncategorized/GameObject.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.doubleagamesdev.uncategorized;
+
+/**
+ *
+ * @author Phil
+ */
+public abstract class GameObject 
+{
+    protected float x, y;   //x and y
+    protected float sx, sy; //sizex and sizey
+    
+    abstract void update();
+    public void render()
+    {
+        Draw.drawRect(x, y, sx, sy);
+    }
+    
+    
+    /** getter for floats **/
+    
+    public float getX()
+    {
+      return x;  
+    }
+    
+    public float getY()
+    {
+      return y;  
+    }
+    
+    public float getSX()
+    {
+      return sx;  
+    }
+    
+    public float getSY()
+    {
+      return sy;  
+    }
+    
+    public float getCenterY()
+    {
+        return y + (sy/2); //create center of GameObject
+    }
+}
