b9a67015617d22fa829600c978a1df225173dc09
Malik Disl <malik.disl@gmx.de>
2014-02-12 23:08:55 +0100

    3DEngine Mesh loading

--- 3DGameEngineMalik/src/com/base/engine/RecourceLoader.java
+++ 3DGameEngineMalik/src/com/base/engine/RecourceLoader.java
@@ -1,7 +1,10 @@
 package com.base.engine;
 
+import com.sun.xml.internal.fastinfoset.util.StringArray;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 /**
  * Created by Malik on 02.02.14.
@@ -27,5 +30,66 @@ public class RecourceLoader {
         }
 
         return shaderSource.toString();
+    }
+    public static Mesh loadMesh(String fileName){
+        String[] splitArray = fileName.split("//.");
+        String ext = splitArray[splitArray.length - 1];
+
+        if(!ext.equals("obj"))
+        {
+            System.err.println("ERROR file formate not supported");
+            new Exception().printStackTrace();
+            System.exit(1);
+        }
+
+        ArrayList<Vertex> vertices = new ArrayList<Vertex>();
+        ArrayList<Integer> indices = new ArrayList<Integer>();
+
+        BufferedReader meshReader = null;
+
+
+        try{
+            meshReader = new BufferedReader(new FileReader("./res/models/" + fileName));
+            String line;
+            while ((line = meshReader.readLine()) != null)
+            {
+                String[] tokens = line.split(" ");
+                tokens = Util.removeEmptyString(tokens);
+
+                if(tokens.length == 0 || tokens[0].equals("#"))
+                    continue;
+                else if(tokens[0].equals("v"))
+                {
+                    vertices.add(new Vertex(new Vector3f(Float.valueOf(tokens[1]),
+                                                         Float.valueOf(tokens[2]),
+                                                         Float.valueOf(tokens[3]))));
+                }
+                else if(tokens[0].equals("f"))
+                {
+                    indices.add(Integer.parseInt(tokens[1]) - 1);
+                    indices.add(Integer.parseInt(tokens[2]) - 1);
+                    indices.add(Integer.parseInt(tokens[3]) - 1);
+
+                }
+            }
+
+            meshReader.close();
+
+            Mesh res = new Mesh();
+            Vertex[] vertexData = new Vertex[vertices.size()];
+            vertices.toArray(vertexData);
+
+            Integer[] indexData = new Integer[indices.size()];
+            indices.toArray(indexData);
+
+            res.addVertices(vertexData, Util.toIntArray(indexData));
+
+            return res;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return null;
     }
 }
